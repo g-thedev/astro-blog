5af09cdc855710253760deeef87db25dce4a45cd
Gerard Fajardo <gerard@zeta.haus>
2024-03-07 17:38:52 +0000

    Update astro config with netlify site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site:"https://fabulous-bombolone-26ee68.netlify.app/"
+});
