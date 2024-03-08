f47f048f5b0f2b14b76c779754825571e8444822
Gerard Fajardo <gerard@zeta.haus>
2024-03-08 03:00:39 +0000

    Install preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site:"https://fabulous-bombolone-26ee68.netlify.app/"
+  site: "https://fabulous-bombolone-26ee68.netlify.app/",
+  integrations: [preact()]
 });
